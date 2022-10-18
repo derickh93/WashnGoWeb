85f98e66a5bbe626d98062428e08718977ed1837
Derick Hansraj <derickhansraj@ymail.com>
2022-10-17 21:09:29 -0400

    fixed center issues

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import ConfirmDetails from "../components/Preferences/ConfirmDetails";
 import { format } from "date-fns";
@@ -9,18 +9,11 @@ import { useSelector } from "react-redux";
 import dryCleanProds from "./product-data/products.json";
 import washProds from "./product-data/product-wash.json";
 
-
-
 import { useHistory } from "react-router-dom";
 
 export default function Confirmation() {
-  const {
-    logout,
-    readProfile,
-    currentUser,
-    customerPortal,
-    checkoutSession
-  } = useAuth();
+  const { logout, readProfile, currentUser, customerPortal, checkoutSession } =
+    useAuth();
 
   const { arr } = useSelector((state) => state.dryClean);
   const { arrWash } = useSelector((state) => state.wash);
@@ -34,50 +27,50 @@ export default function Confirmation() {
   let validSum = 0;
   //////////////////////////////////////////////////////////////////////
   const line_items = [];
-  for(let i = 0; i < arr.length;i++) {
-    if(arr[i] > 0){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
       validSum += parseFloat(dryCleanProds[i].price.substring(1));
-      line_items.push(     {
+      line_items.push({
         price: dryCleanProds[i].price_id,
         adjustable_quantity: {
           enabled: true,
           minimum: 1,
-          maximum: 99
+          maximum: 99,
         },
         quantity: arr[i],
-      });    }
+      });
+    }
   }
 
-
-  for(let i = 0; i < arrWash.length;i++) {
-
-    if(arrWash[i] > 0){
+  for (let i = 0; i < arrWash.length; i++) {
+    if (arrWash[i] > 0) {
       validSum += parseFloat(washProds[i].price.substring(1));
-      line_items.push(     {
+      line_items.push({
         price: washProds[i].price_id,
         adjustable_quantity: {
           enabled: true,
           minimum: 1,
-          maximum: 99
+          maximum: 99,
         },
         quantity: arrWash[i],
-      });    }
+      });
+    }
   }
 
-  if(validSum < 30){
-    const finalSum = 3000-(validSum*100)
+  if (validSum < 30) {
+    const finalSum = 3000 - validSum * 100;
     line_items.push({
-        price_data: {
-          currency: 'usd',
-          unit_amount: finalSum,
-          product_data: {
-            name: 'Minimum',
-            description: '$30 minimum',
-            images: ['https://example.com/t-shirt.png'],
-          },
+      price_data: {
+        currency: "usd",
+        unit_amount: finalSum,
+        product_data: {
+          name: "Minimum",
+          description: "$30 minimum",
+          images: ["https://example.com/t-shirt.png"],
         },
-        quantity: 1
-    })
+      },
+      quantity: 1,
+    });
   }
   /////////////////////////////////////////////////////////////////////
   const userData = JSON.parse(sessionStorage.getItem("stripeInstance"));
@@ -89,7 +82,7 @@ export default function Confirmation() {
     e.preventDefault();
     setLoading(true);
     try {
-      checkoutSession(userData.id,line_items).then((url) => {
+      checkoutSession(userData.id, line_items).then((url) => {
         window.location = url;
       });
     } catch (err) {
@@ -99,9 +92,8 @@ export default function Confirmation() {
   }
 
   async function handlePortal() {
-
     try {
-      customerPortal(userData.id,'confirmation').then((url) => {
+      customerPortal(userData.id, "confirmation").then((url) => {
         window.location = url;
       });
     } catch (err) {
@@ -110,7 +102,6 @@ export default function Confirmation() {
   }
 
   async function handleLogout() {
-
     try {
       await logout()
         .then(() => {
@@ -153,7 +144,7 @@ export default function Confirmation() {
       // dry: dryer,
       // soft: softener,
       addit: additional,
-      arrWashSum : arrWashSum
+      arrWashSum: arrWashSum,
       // whi: whites,
     };
   } catch (err) {
@@ -170,69 +161,82 @@ export default function Confirmation() {
 
   return (
     <div>
-      {loading ?     
-      <div className="homepage">
+      {loading ? (
+        <div className="homepage">
           <img src={animation} alt="loading..." />
-        </div> : <div>
+        </div>
+      ) : (
+        <div>
+          <div className="homepage"></div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
 
-      <div className="homepage">
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-
-          justifyContent: "flex-end",
-        }}
-      >
-        <Button
-          variant="outline-primary"
-          style={{
-            width: "20%",
-            height: "20%",
-            fontSize: "12px",
-            backgroundColor: "transparent",
-            boxShadow: "none",
-          }}
-          onClick={goBack}
-        >
-          <FontAwesomeIcon
-            icon="chevron-circle-left"
-            size="lg"
-            color="#1C2F74"
-          />
-        </Button>{" "}
-        <Button
-          style={{
-            width: "20%",
-            height: "20%",
-            fontSize: "12px",
-            backgroundColor: "transparent",
-            boxShadow: "none",
-          }}
-          variant="link"
-          onClick={handlePortal}
-        >
-          <u>Manage Account</u>
-        </Button>
-        <Button
-          style={{
-            width: "20%",
-            height: "20%",
-            fontSize: "12px",
-            backgroundColor: "transparent",
-            boxShadow: "none",
-          }}
-          variant="link"
-          onClick={handleLogout}
-        >
-          <u>Log Out</u>
-        </Button>
-      </div>
-      <ConfirmDetails commonProps={commonProps}></ConfirmDetails>
-        {commonProps.address !== "N/A" &&
-      <Button className="mt-2" style={{backgroundColor:'#1C2F74'}} onClick={(e) =>{handlePayment(e)}}>PAY</Button>}
-      </div>}
-      </div>
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button
+              variant="outline-primary"
+              style={{
+                width: "20%",
+                height: "20%",
+                fontSize: "12px",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              }}
+              onClick={goBack}
+            >
+              <FontAwesomeIcon
+                icon="chevron-circle-left"
+                size="lg"
+                color="#1C2F74"
+              />
+            </Button>{" "}
+            <Button
+              style={{
+                width: "20%",
+                height: "20%",
+                fontSize: "12px",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              }}
+              variant="link"
+              onClick={handlePortal}
+            >
+              <u>Manage Account</u>
+            </Button>
+            <Button
+              style={{
+                width: "20%",
+                height: "20%",
+                fontSize: "12px",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              }}
+              variant="link"
+              onClick={handleLogout}
+            >
+              <u>Log Out</u>
+            </Button>
+          </div>
+          <ConfirmDetails commonProps={commonProps}></ConfirmDetails>
+          {commonProps.address !== "N/A" && (
+            <div className="d-flex justify-content-center">
+              {" "}
+              <Button
+                className="mt-2"
+                style={{ backgroundColor: "#1C2F74" }}
+                onClick={(e) => {
+                  handlePayment(e);
+                }}
+              >
+                PAY
+              </Button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
